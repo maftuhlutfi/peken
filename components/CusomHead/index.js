b57a319b41bd81c5e21893edca8f9853339cc572
unknown <playmakernyomlo@gmail.com>
2021-12-20 06:00:14 +0700

    add meta image

--- components/CusomHead/index.js
+++ components/CusomHead/index.js
@@ -11,13 +11,13 @@ const CustomHead = ({title, description, url, image}) => {
             <meta property="og:url" content={url} />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
-            <meta property="og:image" content={image || "https://i.ibb.co/zn9Bfj4/more-project.jpg"} />
+            <meta property="og:image" content={image || "https://i.ibb.co/mh7Xt7F/peken-meta.jpg"} />
 
             <meta property="twitter:card" content="summary_large_image" />
             <meta property="twitter:url" content={url} />
             <meta property="twitter:title" content={title} />
             <meta property="twitter:description" content={description} />
-            <meta property="twitter:image" content={image || "https://i.ibb.co/zn9Bfj4/more-project.jpg"} />
+            <meta property="twitter:image" content={image || "https://i.ibb.co/mh7Xt7F/peken-meta.jpg"} />
         </Head>
     );
 }
