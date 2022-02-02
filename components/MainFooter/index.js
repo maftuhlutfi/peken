32860be6feb0c403ade52b16df6a19d5e8bd8a82
maftuhlutfi <playmakernyomlo@gmail.com>
2022-02-02 21:18:50 +0700

    add social link to footer

--- components/MainFooter/index.js
+++ components/MainFooter/index.js
@@ -1,9 +1,38 @@
+import Image from "next/image";
+import Section from "../shared/Section";
+
+const socialList = [
+    {
+        icon: 'mail.svg',
+        href: 'mailto:pekenaja2021@gmail.com'
+    },
+    {
+        icon: 'whatsapp.svg',
+        href: 'https://api.whatsapp.com/send?phone=6285865267592&text=Hai%20admin%20PEKENAJA%2C%20aku%20mau%20tanya%20dong%20tentang%20...'
+    },
+    {
+        icon: 'instagram.svg',
+        href: 'https://www.instagram.com/peken_aja/'
+    },
+    {
+        icon: 'tiktok.svg',
+        href: 'https://www.tiktok.com/@pekenaja_official'
+    },
+]
+
 const MainFooter = () => {
     return (
-        <footer className="bg-text-primary h-20 flex items-center justify-center">
-            <p className="text-center text-white lg:text-lg lg:font-bold">Peken.id@2021</p>
-        </footer>
+        <Section className='bg-text-primary py-8 lg:py-8'>
+            <p className="text-center text-white lg:text-lg lg:font-bold mb-5">pekenaja@2021</p>
+            <div className="flex justify-center">
+                {socialList.map((s, i) =>
+                    <a key={i} href={s.href} className="mr-4 last:mr-0" target='_blank'>
+                        <Image src={`/icons/footer/${s.icon}`} width={32} height={32} />
+                    </a>
+                )}
+            </div>
+        </Section>
     );
 }
- 
+
 export default MainFooter;
